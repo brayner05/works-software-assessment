9cbed6b66f0b17755d826787b52a880984b6a488
Brayden <braydenrayner@outlook.com>
2024-03-31 20:50:49 -0300

    Fixed the bug where creating a new note would load a non-existing note

--- src/components/pages/SelectGroup.tsx
+++ src/components/pages/SelectGroup.tsx
@@ -45,12 +45,18 @@ const SelectGroup = () => {
                 ) : undefined}
 
                 {groups.length > 0 ? (
-                    <select className="block w-full px-3 py-2 bg-transparent rounded-md border-2 border-gray-400 focus:border-blue-500">
+                    <select
+                        onChange={event => {
+                            const selectedGroup = groups.find(
+                                group => group.id === event.target.value
+                            )
+                            SetGroup(selectedGroup || null)
+                        }}
+                        className="block w-full px-3 py-2 bg-transparent rounded-md border-2 border-gray-400 focus:border-blue-500"
+                    >
                         <option>Choose a group</option>
                         {groups.map(group => (
-                            <option onSelect={() => SetGroup(group)}>
-                                {group.name}
-                            </option>
+                            <option value={group.id}>{group.name}</option>
                         ))}
                     </select>
                 ) : (
